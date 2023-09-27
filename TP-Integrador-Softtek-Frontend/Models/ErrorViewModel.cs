2c3f5397c3f3d9e5aff3282b1ea7c840e3aaf4c1
Jesica Fernandez <je.fernandez.cai@gmail.com>
2023-09-27 19:47:13 -0300

    Agregar archivos de proyecto.

---
+++ TP-Integrador-Softtek-Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TP_Integrador_Softtek_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
